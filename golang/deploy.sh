8050ae0c86ae6b47ef2ea4e1e03df46f82b6292d
Keiji, Yoshimi <walf443@gmail.com>
2015-10-31 14:21:55 +0900

    stop start supervisorctl

--- golang/deploy.sh
+++ golang/deploy.sh
@@ -5,5 +5,7 @@ DEPLOY_USER=$1
 scp $DEPLOY_USER@isu11c:~/isucon5_final/golang/app ./app
 for SERVER in isu11a isu11b isu11c
 do
+  ssh -t isucon@$SERVER sudo supervisorctl stop golang
   scp ./app isucon@$SERVER:
+  ssh -t isucon@$SERVER sudo supervisorctl start golang
 done
